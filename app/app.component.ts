55c929b51044a7a3feddb5366b49cddae30a0fc7
Amir Sadeghin <amir.sadeghin@telecomputing.se>
2016-10-31 17:46:06 +0100

    Started adding ability to select player.

--- app/app.component.ts
+++ app/app.component.ts
@@ -17,23 +17,68 @@ const PLAYERS: Player[] = [
       <h1>{{title}}</h1>
       <h2>Players</h2>
       <ul class="players">
-        <li *ngFor="let player of players">
+        <li *ngFor="let player of players" (click)="onSelect(player)">
           <span class="badge">{{player.id}}</span> {{player.name}}
         </li>
       </ul>
-      <div><label>id: </label>{{player.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="player.name" placeholder="name"/>
-      </div>
-      `
+      `,
+  styles: [`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
+
 })
 
 export class AppComponent {
   title = 'Movie List';
   players = PLAYERS;
-  player: Player = {
-    id: 1,
-    name: 'Martin Hong'
-  };
+  selectedPlayer: Player;
+  onSelect(player: Player): void {
+    this.selectedPlayer = player;
+  }
 }

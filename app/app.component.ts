68f288c6ae7c8de8bd7cf6e7aa325c2777c31044
Amir Sadeghin <amir.sadeghin@telecomputing.se>
2016-10-31 16:27:05 +0100

    Added Player class.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,33 @@
 import { Component } from '@angular/core';
 
+export class Player {
+  id: number;
+  name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: `<h1>My First Angular App</h1>`
+    template: `
+      <h1>{{title}}</h1>
+      <h2>Players</h2>
+      <div><label>id: </label>{{player.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="player.name" placeholder="name"/>
+      </div>
+      `
 })
-export class AppComponent { }
+
+const PLAYERS: Player[] = [
+  {id: 1, name: 'Martin Hong'},
+  {id: 2, name: 'Kristofer Sanchez'},
+  {id: 3, name: 'Mikael Rydin'}
+];
+
+export class AppComponent {
+  title = 'Movie List';
+  player: Player = {
+    id: 1,
+    name: 'Martin Hong'
+  };
+}

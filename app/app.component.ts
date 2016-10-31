b5464e89df31268977419873005c3c4f9c789137
Amir Sadeghin <amir.sadeghin@telecomputing.se>
2016-10-31 16:36:03 +0100

    Added mock players.

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,11 +5,22 @@ export class Player {
   name: string;
 }
 
+const PLAYERS: Player[] = [
+  {id: 1, name: 'Martin Hong'},
+  {id: 2, name: 'Kristofer Sanchez'},
+  {id: 3, name: 'Mikael Rydin'}
+];
+
 @Component({
     selector: 'my-app',
     template: `
       <h1>{{title}}</h1>
       <h2>Players</h2>
+      <ul class="players">
+        <li *ngFor="let player of players">
+          <span class="badge">{{player.id}}</span> {{player.name}}
+        </li>
+      </ul>
       <div><label>id: </label>{{player.id}}</div>
       <div>
         <label>name: </label>
@@ -18,14 +29,9 @@ export class Player {
       `
 })
 
-const PLAYERS: Player[] = [
-  {id: 1, name: 'Martin Hong'},
-  {id: 2, name: 'Kristofer Sanchez'},
-  {id: 3, name: 'Mikael Rydin'}
-];
-
 export class AppComponent {
   title = 'Movie List';
+  players = PLAYERS;
   player: Player = {
     id: 1,
     name: 'Martin Hong'

1115062e7db10e0ba6086c4ed85026d190edf980
Amir Sadeghin <amir.sadeghin@telecomputing.se>
2016-11-01 10:27:59 +0100

    Extracted the Player Detail component into its own file.

---
+++ app/player-detail.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core'
+import { Player } from "./player";
+
+@Component({
+  selector: 'my-player-detail',
+  template: `
+    <div *ngIf="player">
+      <h2>{{player.name}} details:</h2>
+      <div><label>id: </label>{{player.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="player.name" placeholder="name"/>
+      </div>
+    </div>
+  `
+})
+
+export class PlayerDetailComponent{
+  @Input()
+  player: Player;
+}

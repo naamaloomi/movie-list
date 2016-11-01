8e99a1c617b0e99bef89929524463d86c2b31483
Amir Sadeghin <amir.sadeghin@telecomputing.se>
2016-11-01 16:14:31 +0100

    Added routing.

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,7 +4,8 @@ import { Component } from '@angular/core'
   selector: 'my-app',
   template:`
     <h1>{{title}}</h1>
-    <my-players></my-players>
+    <a routerLink="/players">Players</a>
+    <router-outlet></router-outlet>
   `
 })
 
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms";
+import { RouterModule}  from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { PlayerDetailComponent } from './player-detail.component'
@@ -10,7 +11,13 @@ import { PlayerService } from "./player.service";
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'players',
+        component: PlayersComponent
+      }
+    ])
   ],
   declarations: [
     AppComponent,

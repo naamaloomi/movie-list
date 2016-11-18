bf432e73ab0c1cd876fa8dc0d2026a5a717e1275
Amir Sadeghin <amir.sadeghin@telecomputing.se>
2016-11-18 14:00:59 +0100

    Updated the dashboard component to get top players.

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core';
+
+import { Player } from './player';
+import { PlayerService } from './player.service';
 
 @Component({
   moduleId: module.id,
@@ -6,4 +9,13 @@ import { Component } from '@angular/core'
   templateUrl: 'dashboard.component.html'
 })
 
-export class DashboardComponent{}
+export class DashboardComponent implements OnInit{
+  players: Player[] = [];
+
+  constructor(private playerService: PlayerService){}
+
+  ngOnInit(): void{
+    this.playerService.getPlayers()
+      .then(players => this.players = players.slice(0, 5));
+  }
+}

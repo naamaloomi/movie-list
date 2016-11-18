2bed7a9d0f7889f4be1b7cd1440586202bdf95bd
Amir Sadeghin <amir.sadeghin@telecomputing.se>
2016-11-18 13:33:34 +0100

    Added link to dashboard in template.

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,7 +4,10 @@ import { Component } from '@angular/core'
   selector: 'my-app',
   template:`
     <h1>{{title}}</h1>
-    <a routerLink="/players">Players</a>
+    <nav>
+      <a routerLink="/dashboard">Dashboard</a>
+      <a routerLink="/players">Players</a>
+    </nav>
     <router-outlet></router-outlet>
   `
 })
